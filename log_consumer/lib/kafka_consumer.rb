46f9872769eed2fec6ca27f6f0074bc764cec0a5
James Ebentier <jebentier@invoca.com>
2017-03-17 11:41:31 -0700

    base consumer now working

--- log_consumer/lib/kafka_consumer.rb
+++ log_consumer/lib/kafka_consumer.rb
@@ -7,12 +7,17 @@ class KafkaConsumer
     new(Kafka.new(seed_brokers: seed_brokers))
   end
 
-  def consume(group_id:, topic_id:)
-    
+  def consume(group_id:, topic_id:, &block)
+    consumer = connection.consumer(group_id: group_id)
+    consumer.subscribe(topic_id)
+
+    consumer.each_message do |message|
+      block.call(message)
+    end
   end
 
   private
 
-  attr_reader :kafka_connection
+  attr_reader :connection
 
 end
--- logstash/demo_logger.sh
+++ logstash/demo_logger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-  echo "66.249.65.159 - - [$(date +%Y-%m-%d:%H:%M:%S)] \"GET /api/2016-10-01/1/advertisers/2/advertiser_campaigns/3.json HTTP/1.1\" 404 177 \"-\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X)\""
+  echo "66.249.65.159 - - [$(date +%Y-%m-%d:%H:%M:%S)] \"GET /api/2016-10-01/1/advertisers/2/advertiser_campaigns/3.json HTTP/1.1\" 404 177 \"-\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X)\"" >> /var/log/test.log
   sleep 1
 done
