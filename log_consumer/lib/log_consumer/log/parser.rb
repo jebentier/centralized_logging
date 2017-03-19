a2d74fcbf8f0d93b30f62dc3fe7a8023dcb41892
Peter Wilson <peter@invoca.com>
2017-03-18 22:53:45 -0700

    Add list of regexes for various api endpoints

--- log_consumer/lib/log_consumer/log/parser.rb
+++ log_consumer/lib/log_consumer/log/parser.rb
@@ -19,8 +19,17 @@ module LogConsumer
                   body = { "message" => message }
                   config[:split].each do |k, regex|
                     if thing_to_split = body[k.to_s]
-                      match = thing_to_split.match(regex)
-                      body.merge!(Hash[ match.names.zip( match.captures ) ])
+                      if regex.is_a?(Array) # regex is an ordered list of highest -> lowest precedent patterns to match. Take the first that matches.
+                        regex.find do |pattern|
+                          if match = thing_to_split.match(regex)
+                            body.merge!(Hash[ match.names.zip( match.captures ) ])
+                          end
+                          !!match
+                        end
+                      else  # regex is a single pattern, match it against thing_to_split.
+                        match = thing_to_split.match(regex)
+                        body.merge!(Hash[ match.names.zip( match.captures ) ])
+                      end
                     end
                   end
 
@@ -41,8 +50,34 @@ module LogConsumer
         { all: [{ match: /api\//,
                   index: "logstash-central-logging",
                   split: {
-                             message: /(?<ip_address>(\d{1,3}\.){3}\d{1,3}).*\[(?<timestamp>.*)\].*(?<request_method>(GET|POST|PUT|DELETE))\s*(?<request_path>[^.]*)\.(?<request_format>\w+).*(?<response_code>\d{3}).*(?<response_time_ms>\d+)\s*"(?<referrer>[^"]*)"\s"(?<requesting_client>[^"]*)"/,
-                             request_path: /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>\d+)\/advertiser_campaigns\/(?<advertiser_campaign_id>\d+)/
+                             message: /(?<load_balancer_ip_address>(\d{1,3}\.){3}\d{1,3})\s[^\s]*\s.*(?<user_ip_address>(\d{1,3}\.){3}\d{1,3}).*\[(?<timestamp>.*)\]\s*"(?<request_domain>([^"]*))".*(?<request_method>(GET|POST|PUT|DELETE))\s*(?<request_path>[^.]*)\.(?<request_format>\w+)?(\?(?<request_query>([^\s]*)))[^"]*"\s+(?<response_code>\d{3}).*/,
+                             request_paths: [
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)\/affiliates\/(?<affiliate_id>[^\/]*)\/affiliate_campaigns/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)\/affiliate_campaigns/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)\/promo_numbers\/(?<promo_number>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)\/promo_numbers/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)\/ring_pools\/(?<ring_pool_id>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)\/ring_pools/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns\/(?<advertiser_campaign_id>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/advertiser_campaigns/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/users\/(?<user_id>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/users/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/affiliates\/(?<affiliate_id>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)\/affiliates/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers\/(?<advertiser_id>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/advertisers/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/affiliates\/(?<affiliate_id>[^\/]*)/,
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/affiliates/,
+
+                                              /\/api\/(?<api_version>[^\/]*)\/(?<network_id>\d+)\/network/
+                                            ]
                          }
                 }]
         }
